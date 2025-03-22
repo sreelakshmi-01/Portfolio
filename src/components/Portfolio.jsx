15a172f08ec6640f93c69c72ef873dd64c827915
Sreelakshmi S <sreelakshmis112001@gmail.com>
2025-03-22 13:05:34 +0530

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -87,7 +87,7 @@ The backend, powered by Node.js and Socket.io, manages room creation, user conne
     {
       id: 6,
       title: 'PlotPoint: Books Website',
-      description: 'Developed PlotPoint, an online book-selling platform with user authentication, cart, wishlist, and admin book management (CRUD). Built a scalable backend using Django, ensuring efficient data handling with MySQL. Implemented secure user authentication and session management, improving platform security and usability. Designed a structured database schema to handle book details, categories, and order processing.'
+      description: 'Developed PlotPoint, an online book-selling platform with user authentication, cart, wishlist, and admin book management (CRUD). Built a scalable backend using Django, ensuring efficient data handling with MySQL. Implemented secure user authentication and session management, improving platform security and usability. Designed a structured database schema to handle book details, categories, and order processing.',
       thumbnail: pp1,
       images: [pp1],
       githubUrl: 'https://github.com/sreelakshmi-01/PlotPoint',
