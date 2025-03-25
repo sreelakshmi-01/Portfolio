482d9940914524f68074b9fbee26643f3515574f
Sreelakshmi S <sreelakshmis112001@gmail.com>
2025-03-25 12:32:14 +0530

    mail added to contact

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -221,7 +221,7 @@ const ContactMe = () => {
       </ContactContent>
       <SocialIcons>
         <IconLink href="https://www.linkedin.com/in/sreelakshmi01" target="_blank"><FaLinkedin /></IconLink>
-        <IconLink href="https://wa.me/+918137866551" target="_blank"><FaWhatsapp /></IconLink>
+        <IconLink href="mailto:sreelakshmis112001@gmail.com" target="_blank"><FaEnvelope /></IconLink>
         <IconLink href="https://www.instagram.com/sreelakshmi_s01/" target="_blank"><FaInstagram /></IconLink>
         <IconLink href="https://github.com/sreelakshmi-01" target="_blank"><FaGithub /></IconLink>
       </SocialIcons>
